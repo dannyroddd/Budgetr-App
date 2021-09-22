c81526fcce5fa27c870d19672dd1814e8afd5c6d
Daniel Rodriguez <danielrodriguez@Daniels-MBP.home>
2021-09-22 15:47:08 -0400

    Adds a new route page to add an item to the index of budget items

--- server.js
+++ server.js
@@ -3,24 +3,37 @@ const app = express();
 
 const budget = require('./models/budget')
 
+
+
 app.use(express.urlencoded({ extended: false}))
 app.use(express.static('public'))
 
 
 
+
+
 app.get('/budget', (req, res) => {
-    res.render('index.ejs', {allBudgets: budget});
-});
+    res.render('index.ejs', {allBudgets: budget})
+})
+
+app.get('/budget/new', (req, res)=>{
+    res.render('new.ejs')
+})
+
+app.post('/budget', (req, res)=>{
+    budget.push(req.body)
+    res.redirect('/budget')
+})
 
 
 app.get('/budget/:indexOfBudgetArray', (req, res) => {
-    res.render("show.ejs", {budgets: budget[req.params.indexOfBudgetArray]});
-  });
+    res.render("show.ejs", {budgets: budget[req.params.indexOfBudgetArray]})
+  })
 
 
 
 
 
 app.listen(3000, () => {
-    console.log('listening');
+    console.log('listening')
 });
