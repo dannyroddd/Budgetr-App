3784a0b646e7f0985bd12ce9a9e9946163f18c8a
Daniel Rodriguez <danielrodriguez@Daniels-MBP.home>
2021-09-21 20:44:52 -0400

    Displays data on the index as a table

--- server.js
+++ server.js
@@ -6,12 +6,10 @@ const budget = require('./models/budget')
 app.use(express.urlencoded({ extended: false}))
 app.use(express.static('public'))
 
-app.get('/', (req, res)=>{
-    res.send('Hello Adult Life')
-})
 
-app.get('/test/', (req, res) => {
-    res.render('index.ejs');
+
+app.get('/', (req, res) => {
+    res.render('index.ejs', {allBudgets: budget});
 });
 
 
